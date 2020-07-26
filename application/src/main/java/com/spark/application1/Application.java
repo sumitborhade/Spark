70edf1d4e0ca9e66bc8b47b3f0687c3947d9a65a
Sumit Borhade <sumitborhade@gmail.com>
2020-07-26 23:22:44 +0800

    Adding readme data

--- application/src/main/java/com/spark/application1/Application.java
+++ application/src/main/java/com/spark/application1/Application.java
@@ -8,7 +8,6 @@ import java.util.Scanner;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.types.DataTypes;
 
 public class Application {
 
@@ -16,22 +15,30 @@ public class Application {
 	}
 
 	public static void main(String[] args) {
-		System.setProperty("hadoop.home.dir", "F:\\Spark\\Spark\\Utils");
-		SparkSession sparkSession = SparkSession.builder().appName("First Spark App").master("local[3]").getOrCreate();
+		System.setProperty("hadoop.home.dir", "F:\\Spark\\Spark\\Utils\\");
+		
+		SparkSession sparkSession 
+				= SparkSession.builder()
+					.appName("First Spark App")
+					.master("local[3]")
+					.getOrCreate();
 
 		Dataset<Row> df = sparkSession.read()
+				.format("csv")
 				.option("header", "true")
 				.option("inferSchema", "true")
-				.csv("src/main/resources/Sample.csv");
+				.load("src/main/resources/Sample.csv");
 		
 		Dataset<Row> df1 = df.select("Emp ID", "First Name", "Last Name", "Date of Birth")
 				.withColumnRenamed("Date of Birth", "DOB")
 				.withColumn("DOB", to_date(col("DOB"),"M/d/y"));
+
 		df1.printSchema();
 		df1.show();
 //		df1.show((int)df.count(), false);
 
 		Scanner scanner = new Scanner(System.in);
+//		Uncomment the code for debugging and checking Spark UI
 //		scanner.nextLine();
 		
 		scanner.close();
